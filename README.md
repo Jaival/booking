d299ecd966aa79e7b133d80961beb6c63e948c31
Jaival <saijajaival@gmail.com>
2024-08-25 20:09:19 +0530

    init: initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# BOOKING
+
+### This is a project for my learning and experiments.
+
+I will be learning and experimenting with:
+
+- Auth
+- API's
+- Prisma
+- Database
+- Git/ GitHub CLI
+- and more
+
+> NOTE: Currently this project is an empty project. I will be adding the features while learning so they can be dumb way to implement them.
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex items-center flex-col justify-center gap-4">
+        <h1 className="text-4xl font-bold">Streamline Your Schedule with Seamless Appointment Booking</h1>
+        <p className="text-lg">
+        Effortless Management, Anytime, Anywhere
+        </p>
+        <Button className="text-xl" variant={"default"}>Get Started</Button>
+      </div>
+    </main>
+  );
+}
